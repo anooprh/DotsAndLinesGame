a2e4a263205d11ef58ffadbeaa362ee5752391fa
anooprh <anoop.hallur@gmail.com>
2014-07-01 13:50:06 -0400

    something working

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+var shortid = require('shortid');
 var express = require('express');
+var _ = require('underscore');
+
 var app = express();
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 app.use(express.static(__dirname + '/'));
 console.log('Express server started on port 3000');
+
+var clients = {};
+var gamers = {};
+
 io.sockets.on('connection', function (socket) {
+    socket.id = shortid.generate();
+    clients[socket.id] = socket;
+    if (_.size(clients) === 2) {
+
+        var gamer1 = clients[_.keys(clients)[0]];
+        var gamer2 = clients[_.keys(clients)[1]];
+        delete clients[_.keys(clients)[0]];
+        delete clients[_.keys(clients)[1]];
+
+        gamer1['opponent'] = gamer2;
+        gamer2['opponent'] = gamer1;
+
+        gamer1.emit('matchFound', gamer1['opponent'].id);
+        gamer2.emit('matchFound', gamer2['opponent'].id);
+
+        gamers[gamer1.id] = gamer1;
+        gamers[gamer2.id] = gamer2;
+    }
+
+    socket.emit('setId', socket.id);
+
     socket.on('userClick', function (data) {
-        console.log(data);
+//        console.log(data);
+        var userId = data.socketId;
+        gamers[userId].emit('updateBlock', data);
+        gamers[userId]['opponent'].emit('updateBlock', data);
     });
 });
--- main.js
+++ main.js
@@ -1,17 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var url = window.location.protocol + "//" + window.location.hostname + window.location.port;
-    var socket = io.connect(url);
+    var url = window.location.protocol + "//" + window.location.hostname;
+    var socket = null;
+    var socketId = null;
+    $('.overlay').hide();
+    $('.modal').hide();
 
-    $('.horizontal-line, .vertical-line').click(function(){
+    $('.start-btn').click(function () {
+        $('.overlay').show();
+        $('.modal').show();
+        socket = io.connect(url);
+        socket.on('setId', function (data) {
+            socketId = data;
+        });
+        socket.on('matchFound', function (data) {
+            $('.overlay').hide();
+            $('.modal').hide();
+        });
+        socket.on('updateBlock', function (data) {
+            console.log(data);
+            var block = $("div[r1="+data.r1+"][c1="+data.c1+"][r2="+data.r2+"][c2="+data.c2+"]");
+            if(data.socketId === socketId){
+                block.css('background-color', 'black');
+            } else {
+                block.css('background-color', 'red');
+            }
+
+            block.attr('clicked', 'yes');
+
+        });
+    });
+
+
+    $('.horizontal-line, .vertical-line').click(function () {
         var r1 = $(this).attr('r1');
         var c1 = $(this).attr('c1');
         var r2 = $(this).attr('r2');
         var c2 = $(this).attr('c2');
 
-        var edgeId = r1+','+c1+','+r2+','+c2;
-        console.log("Id ->  " + edgeId);
-        $(this).css('background-color', 'black');
-        socket.emit('userClick',edgeId);
+        var edgeId = {'socketId': socketId, 'r1': r1, 'c1': c1, 'r2': r2, 'c2': c2};
+        socket.emit('userClick', edgeId);
     });
+
+    $('.horizontal-line, .vertical-line').hover(
+        function () {
+            if ($(this).attr('clicked') !== 'yes') {
+                $(this).css('background-color', 'grey');
+            }
+        },
+        function () {
+            if ($(this).attr('clicked') !== 'yes') {
+                $(this).css('background', 'transparent');
+            }
+        });
 });
