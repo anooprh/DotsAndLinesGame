41d1cfec5407e692ef65e28db68ad62e4513e4df
anooprh <anoop.hallur@gmail.com>
2014-07-01 01:25:19 -0400

    removed hardcoding of ip address

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-    var socket = io.connect('http://128.237.168.224:3000');
+
+    var url = window.location.protocol + "//" + window.location.hostname;
+    var socket = io.connect(url);
 
     $('.horizontal-line, .vertical-line').click(function(){
         var r1 = $(this).attr('r1');
         var c1 = $(this).attr('c1');
         var r2 = $(this).attr('r2');
         var c2 = $(this).attr('c2');
+
         var edgeId = r1+','+c1+','+r2+','+c2;
         console.log("Id ->  " + edgeId);
         $(this).css('background-color', 'black');
